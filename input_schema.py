5bfd2fb23c9cd919a9e900a9cb5fdd29d1aea5ca
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-06-12 02:22:42 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,13 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["Penguins having dinner"]
+        'example': ["movie still, ocean, island, (clouds:0.4), epic, film grain"]
+    },
+    "negative_prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["(octane render, render, drawing, anime, bad photo, bad photography:1.3), (worst quality, low quality, blurry:1.2), (bad teeth, deformed teeth, deformed lips), (bad anatomy, bad proportions:1.1), (deformed iris, deformed pupils), (deformed eyes, bad eyes), (deformed face, ugly face, bad face), (deformed hands, bad hands, fused fingers), morbid, mutilated, mutation, disfigured"]
     }
+    
 }
